2f2235b47ffe131054a8f10a18323d501d53e951
QuyLam <qlam@ucsc.edu>
2016-02-19 23:54:29 -0800

    commit

--- econ 294 assignment 4.R
+++ econ 294 assignment 4.R
@@ -2,14 +2,18 @@
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 0 
+
 print("Quy Lam")
 print(1505123)
 print("qlam@ucsc.edu")
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# 1
+# 1 
+
 library(foreign)
+
+## Load files from github
 
 flights <- read.csv("https://github.com/EconomiCurtis/econ294_2015/raw/master/data/flights.csv",
   stringsAsFactors = FALSE,
@@ -17,19 +21,19 @@ flights <- read.csv("https://github.com/EconomiCurtis/econ294_2015/raw/master/da
 )
 
 planes <- read.csv(
-  "https://github.com/EconomiCurtis/econ294_2015/blob/master/data/planes.csv",
+  "https://github.com/EconomiCurtis/econ294_2015/raw/master/data/planes.csv",
   stringsAsFactors = FALSE,
   header = T
 )
 
 airports <- read.csv(
-  "https://github.com/EconomiCurtis/econ294_2015/blob/master/data/airports.csv",
+  "https://github.com/EconomiCurtis/econ294_2015/raw/master/data/airports.csv",
   stringsAsFactors = FALSE,
   header = T
 )
 
 weather <- read.csv(
-  "https://github.com/EconomiCurtis/econ294_2015/blob/master/data/weather.csv",
+  "https://github.com/EconomiCurtis/econ294_2015/raw/master/data/weather.csv",
   stringsAsFactors = FALSE,
   header = T
 )
@@ -41,7 +45,7 @@ require(dplyr)
 #convert date column from type char to type date
 
 weather <- weather %>%
-  mutate(date = as.Date())
+  mutate(date = as.Date(date))
 
 flights <- flights %>%
   mutate(date = as.Date(date))
@@ -49,14 +53,18 @@ flights <- flights %>%
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 3
 
+#all flights went to city of San Francisco & Oakland CA 
+
 flights.2a <- subset(flights, dest == "SFO" | dest == "OAK")
 print(nrow(flights.2a))
 
+#all flights delayed by an hour or more
 
-flights.2b <- subset(flights, dep_delay + arr_delay >= 60)
+flights.2b <- subset(flights, pdep_delay + arr_delay >= 60)
 print(nrow(flights.2b))
 
-flights.2c <- subset(flights)
+#all flights which arrival delay was more than twice departure delay
+flights.2c <- subset(flights, arr_delay > ((2)*dep_delay))
 
 day_delay <- dplyr::filter( summarize(
   group_by( dplyr::filter(
@@ -69,35 +77,22 @@ n > 10 )
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 4
-flights.3a <- flights %>%
+
+flights.4a <- flights %>%
   dplyr::select(ends_with("delay"))
 
-flights.3b <- flights %>%
+flights.4b <- flights %>%
   dplyr::select(starts_with("arr_"),starts_with ("dep_"))
 
-flights.3c <- flights %>%
+flights.4c <- flights %>%
   dplyr::select(dep_delay:arr_delay)
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 5
 flights.5a <- flights %>%
   dplyr::arrange(desc(dep_delay))
-  
 print(head(flights.5a,5))
 
-
-{caughtup_time<- function(x,y){
-  c <- (x+y)
-  ifelse(c < 0,abs(c),-c)
-}
-}
-
-
-
-{flights.5b <- flights %>%
-  dplyr::mutate(
-    caughtup_lost = caughtup_time(arr_delay,dep_delay)
-  )
-}
 
 flights.5b <- flights %>%
   dplyr::mutate(
@@ -109,40 +104,27 @@ print(head(flights.5b,5))
 
 
 
-
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 6
- ### HOW TO DEAL WITH NA??? ######
-{speed <- function(x,y,z){
-  speed = (z/(x + y/60))
-}
-}
-{delta <- function(x,y){
-  delta = x +y 
-  ifelse(delta< 0,abs(delta),-delta)
-}}
 
-{flights.6 <- flights %>%
-  dplyr::mutate(
-    speed = speed(hour,minute,dist),
-    delta = delta(dep_delay - arr_delay)
-  )
-}
 flights.6 <-mutate(flights,
     speed = dist /(hour + minute/60),
     delta = dep_delay-arr_delay
   )
-View(flights.6)
 
+##6a##
 print(flights.6 %>%
   dplyr::arrange(desc(speed)) %>%
   head(5)
 )
+
+##6b##
 print(flights.6 %>%
   dplyr::arrange(desc(delta)) %>%
   head(5)
 )
 
+##6c##
 print(flights.6 %>%
   dplyr::arrange(delta) %>%
   head(5)
@@ -151,6 +133,8 @@ print(flights.6 %>%
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 7
+
+#Grouping by carrier
 
 flights.7a <- flights.6 %>%
   dplyr::group_by(carrier) %>%
@@ -173,15 +157,7 @@ print(flights.7a %>%
 )
 
 flights.7b <- flights.6 %>%
-  dplyr::group_by(date,hour) %>%
-  
-  
-  
-
-#######CODE EXPLANATION ?????????#####
-{flights.7b <- flights.6 %>%
-  dplyr::group_by(date,hour) %>%
-}  
+  dplyr::group_by(date,hour) 
   
 day_delay <- flights %>%
   filter(!is.na(dep_delay)) %>%
@@ -189,27 +165,8 @@ day_delay <- flights %>%
   summarize(delay = mean(dep_delay),n = n()) %>%
   filter(n > 10)
 
-
-
-
-
-
-{# for each carrier, calculate the percentage of flights cancelled or diverted
-b <- flights.6 %>%
-  group_by(carrier) %>%
-  summarise_each(funs(mean), cancelled)
-
-# for each carrier, calculate the minimum and maximum arrival and departure delays
-flights %>%
-  group_by(UniqueCarrier) %>%
-  summarise_each(funs(min(., na.rm=TRUE), max(., na.rm=TRUE)), matches("Delay"))
-}
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 8
-
-
-
-
 
 day_delay <- day_delay %>%
   mutate(diff = delay - lag(delay))
@@ -220,26 +177,9 @@ print(day_delay %>%
 )
 
 
-{day_delay <- flights %>%
-  dplyr::filter(
-    summarize(
-      group_by(
-        dplyr::filter(
-        flights,
-        !is.na(dep_delay)
-      ),
-    date
-    ),
-    delay = mean(dep_delay),
-    n = n()
-    ),
-  n > 10
-)
-}
-
-
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# 9   ?????????????????????????? GROUP BY DEST OR DEST + ARR
+# 9
+
 dest_delay <- flights %>%
   group_by(dest) %>%
   summarise(
@@ -253,23 +193,23 @@ airports <- airports %>%
 df.9a <- dest_delay %>%
   left_join(airports,
             by = "dest")
-
-###dont get it
+#print out 5 city & state
 df.9a1 <- df.9a %>%
         arrange(desc(average_arrde)) %>% 
         head(5)
 
-print(df.9a1$city)
+print(data.frame(df.9a1$city,df.9a1$state))
+
 
 df.9b <- dest_delay %>% inner_join(airports)
-print("number of observation same or not?")
-df.9c <- dest_delay %>% right_join(airports) 
-print("NUMBER OF OBSERVATION +there are NAs in df.9c because right_join include all observations in airport which are match or not match in dest_delay.NAs is observation available in airports but dest_delay doesn't have")
+print("2 data have same number of observations")
+df.9c <- dest_delay %>% right_join(airports)
+print(nrow(df.9c))
+print("there are 3376 observations in data set +there are NAs in df.9c because right_join include all observations in airport which are match or not match in dest_delay.NAs is observation available in airports but dest_delay doesn't have")
 
 df.9d <- dest_delay %>% full_join(airports) 
-print("NUMBER OF OBSERVATION +there are NAs in df.9d,")
-
-
+print(nrow(df.9d))
+print("there are 3378 observations in dataset +there are NAs in df.9d,2 dataset join whether observation matching perfectly or not")
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 11
@@ -323,9 +263,10 @@ df <- df %>%
     demo = c("f.11.DC", "f.55.NY", "m.65.WA", NA),
     value = c(3,4,5,6)
   )
+  
 df2 <- df %>%
     unite(
       col = demo, 
-      ... = sex, age, city, # ... all columns that we want to combine together
-      sep = "." # how we want to seperate them (with single "_")
+      ... = sex, age, city, 
+      sep = "." 
     ) 
